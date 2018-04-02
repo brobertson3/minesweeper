aea96426b96ee0ed8f94e5fe8226575db3520ea6
brobertson3 <brobertson3@users.noreply.github.com>
2018-04-01 19:14:15 -0700

    Added function that prints the hardcoded board to the console.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,11 +1,25 @@
-const blankLine = '  |   |  ';
-const guessLine = '1 |   |  ';
-const bombLine  = '  | B |  ';
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+/* Function to handle formatting the board for printing to the console.
+ * This uses the arrow function notation from ES6, omitting the set of
+ * parentheses around the parameter since there is only a single parameter.
+ */
+const printBoard = board => {
+	console.log('Current Board:');
+	console.log(board[0].join(" | "));
+	console.log(board[1].join(" | "));
+	console.log(board[2].join(" | "));
+}
+
+let board = [
+	[' ', ' ', ' '], 
+	[' ', ' ', ' '], 
+	[' ', ' ', ' ']
+];
+
+printBoard(board);
+
+// Hardcode indexes of the board array
+board[0][1] = '1';
+board[2][2] = 'B';
+
+printBoard(board);
+

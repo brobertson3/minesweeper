a1fea52f4472ce0c283f411e8c53359fc8f0e607
brobertson3 <brobertson3@users.noreply.github.com>
2018-04-06 14:49:16 -0700

    Features - detect neighboring bombs

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -37,14 +37,60 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
 		randomRowIndex = Math.floor(Math.random() * numberOfRows);
 		randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
 
-		board[randomRowIndex][randomColumnIndex] = 'B';
+		// Check to see if bomb space free. If free then place bomb, else try again
+		if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+			board[randomRowIndex][randomColumnIndex] = 'B';
+			numberOfBombsPlaced++;
+		}
 
-		numberOfBombsPlaced++;
-
-		//TODO - restrict bombs from being placed at the same index
 	}
 
 	return board;
+}
+
+/*
+ * Function to check how many adjacent bombs there are to the user's choice.
+ */
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+
+	const neighborOffsets = [[-1, -1], [-1, 0], [-1, 1],
+	                         [0, -1], [0, 1],
+	                         [1, -1], [1, 0], [1, 1]];
+
+    const numberOfRows = bombBoard.length;
+    const numberOfColumns = bombBoard[0].length;
+    let numberOfBombs = 0;
+
+    neighborOffsets.forEach(offset => {
+    	const neighborRowIndex = offset[0] + rowIndex;
+    	const neighborColumnIndex = offset[1] + columnIndex;
+
+    	// If neighbor is on the board and contains a bomb then increment numberOfBombs
+    	if (neighborRowIndex < numberOfRows && neighborRowIndex >= 0 &&
+    		neighborColumnIndex < numberOfColumns && neighborColumnIndex >= 0) {
+    		if (bombBoard[neighborRowIndex][neighborColumnIndex] === 'B') {
+    			numberOfBombs++;
+    		}
+    	}
+    });
+
+    return numberOfBombs;
+}
+
+/*
+ * User can choose a tile to flip and update that tile with either game over or
+ * the number of surrounding bombs.
+ */
+const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
+	if (playerBoard[rowIndex][columnIndex] !== ' ') {
+		console.log('This tile has already been flipped!');
+		return;
+	} else if (bombBoard[rowIndex][columnIndex] === 'B') {
+		playerBoard[rowIndex][columnIndex] = 'B';
+		//TODO - I feel like this should be game over
+	} else {
+		playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard, rowIndex, columnIndex);
+	}
 }
 
 /* Function to handle formatting the board for printing to the console.
@@ -71,4 +117,8 @@ console.log('-- Player Board --')
 printBoard(playerBoard);
 console.log('--- Bomb Board ---')
 printBoard(bombBoard);
+
+flipTile(playerBoard, bombBoard, 0 , 0);
+console.log('Updated Player Board: ');
+printBoard(playerBoard);
 

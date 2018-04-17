af0050227488a0830a7357825d115d8f10d82532
brobertson3 <brobertson3@users.noreply.github.com>
2018-04-17 00:10:47 -0700

    Moved all of the functionality and properties into classes

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,124 +1,154 @@
-// Function declaration to dynamically create blank board to hold player's guesses
-const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
-	let board = [];
-
-	for (let i = 0; i < numberOfRows; i++) {
-		let row = [];
-
-		for (let j = 0; j < numberOfColumns; j++) {
-			row.push(' ');
-		}
-
-		board.push(row);
+class Game {
+	constructor (numberOfRows, numberOfColumns, numberOfBombs) {
+		this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
 	}
 
-	return board;
-};
-
-// Function declaration to dynamically create blank board to hold bomb locations
-const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
-	let board = [];
-
-	for (let i = 0; i < numberOfRows; i++) {
-		let row = [];
-
-		for (let j = 0; j < numberOfColumns; j++) {
-			row.push(null);
+	playMove (rowIndex, columnIndex) {
+		this._board.flipTile(rowIndex, columnIndex);
+		/* 
+		 * Check to see if the user won, lost, or is still playing
+		 */
+		if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
+			console.log("Game over man!");
+			this._board.print();
+		} else if (!this._board.hasSafeTiles()) {
+			console.log("Nice work. You've won!");
+		} else {
+			console.log('Current Board:')
+			this._board.print();
 		}
-
-		board.push(row);
 	}
-
-	let numberOfBombsPlaced = 0;
-	let randomRowIndex = 0;
-	let randomColumnIndex = 0;
-
-	while (numberOfBombsPlaced < numberOfBombs) {
-		randomRowIndex = Math.floor(Math.random() * numberOfRows);
-		randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-
-		// Check to see if bomb space free. If free then place bomb, else try again
-		if (board[randomRowIndex][randomColumnIndex] !== 'B') {
-			board[randomRowIndex][randomColumnIndex] = 'B';
-			numberOfBombsPlaced++;
-		}
-
-	}
-
-	return board;
 }
 
-/*
- * Function to check how many adjacent bombs there are to the user's choice.
- */
-const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+class Board {
+	constructor (numberOfRows, numberOfColumns, numberOfBombs) {
+		this._numberOfBombs = numberOfBombs;
+		this._numberOfTiles = numberOfRows * numberOfColumns;
+		this._playerBoard = Board.generatePlayerBoard(numberOfRows, numberOfColumns);
+		this._bombBoard = Board.generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs);
+	}
 
-	const neighborOffsets = [[-1, -1], [-1, 0], [-1, 1],
-	                         [0, -1], [0, 1],
-	                         [1, -1], [1, 0], [1, 1]];
+	get playerBoard () {
+		return this._playerBoard;
+	}
 
-    const numberOfRows = bombBoard.length;
-    const numberOfColumns = bombBoard[0].length;
-    let numberOfBombs = 0;
+	/*
+ 	 * User can choose a tile to flip and update that tile with either game over or
+ 	 * the number of surrounding bombs.
+ 	 */
+	flipTile (rowIndex, columnIndex) {
+		if (this._playerBoard[rowIndex][columnIndex] !== ' ') {
+			console.log('This tile has already been flipped!');
+			return;
+		} else if (this._bombBoard[rowIndex][columnIndex] === 'B') {
+			this._playerBoard[rowIndex][columnIndex] = 'B';
+			//TODO - I feel like this should be game over
+		} else {
+			this._playerBoard[rowIndex][columnIndex] = this.getNumberOfNeighborBombs(rowIndex, columnIndex);
+		}
+		this._numberOfTiles--;
+	}
 
-    neighborOffsets.forEach(offset => {
-    	const neighborRowIndex = offset[0] + rowIndex;
-    	const neighborColumnIndex = offset[1] + columnIndex;
+	/*
+ 	 * Function to check how many adjacent bombs there are to the user's choice.
+ 	 */
+	getNumberOfNeighborBombs (rowIndex, columnIndex) {
 
-    	// If neighbor is on the board and contains a bomb then increment numberOfBombs
-    	if (neighborRowIndex < numberOfRows && neighborRowIndex >= 0 &&
-    		neighborColumnIndex < numberOfColumns && neighborColumnIndex >= 0) {
-    		if (bombBoard[neighborRowIndex][neighborColumnIndex] === 'B') {
-    			numberOfBombs++;
+		const neighborOffsets = [[-1, -1], [-1, 0], [-1, 1],
+	                 	        [0, -1], [0, 1],
+	                     	    [1, -1], [1, 0], [1, 1]];
+
+    	const numberOfRows = this._bombBoard.length;
+    	const numberOfColumns = this._bombBoard[0].length;
+    	let numberOfBombs = 0;
+
+    	neighborOffsets.forEach(offset => {
+    		const neighborRowIndex = offset[0] + rowIndex;
+    		const neighborColumnIndex = offset[1] + columnIndex;
+
+    		// If neighbor is on the board and contains a bomb then increment numberOfBombs
+    		if (neighborRowIndex < numberOfRows && neighborRowIndex >= 0 &&
+    			neighborColumnIndex < numberOfColumns && neighborColumnIndex >= 0) {
+    			if (this._bombBoard[neighborRowIndex][neighborColumnIndex] === 'B') {
+    				numberOfBombs++;
+    			}
     		}
-    	}
-    });
+    	});
 
-    return numberOfBombs;
-}
+    	return numberOfBombs;
+	}
 
-/*
- * User can choose a tile to flip and update that tile with either game over or
- * the number of surrounding bombs.
- */
-const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
-	if (playerBoard[rowIndex][columnIndex] !== ' ') {
-		console.log('This tile has already been flipped!');
-		return;
-	} else if (bombBoard[rowIndex][columnIndex] === 'B') {
-		playerBoard[rowIndex][columnIndex] = 'B';
-		//TODO - I feel like this should be game over
-	} else {
-		playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard, rowIndex, columnIndex);
+	hasSafeTiles () {
+		return (this._numberOfTiles !== this._numberOfBombs);
+	}
+
+	/* Function to handle formatting the board for printing to the console.
+ 	 * This uses the arrow function notation from ES6, omitting the set of
+ 	 * parentheses around the parameter since there is only a single parameter.
+ 	 * Here 'row' is an array of spaces, so join works on it. The chained 'join'
+ 	 * call works to put newline char between rows.
+ 	 */
+	print () {
+		console.log(this._playerBoard.map(row => {
+			return row.join(' | ');
+		}).join('\n'));
+	}
+
+	// Function declaration to dynamically create blank board to hold player's guesses
+	static generatePlayerBoard (numberOfRows, numberOfColumns) {
+		let board = [];
+
+		for (let i = 0; i < numberOfRows; i++) {
+			let row = [];
+
+			for (let j = 0; j < numberOfColumns; j++) {
+				row.push(' ');
+			}
+
+			board.push(row);
+		}
+
+		return board;
+	}
+
+	// Function declaration to dynamically create blank board to hold bomb locations
+	static generateBombBoard (numberOfRows, numberOfColumns, numberOfBombs) {
+		let board = [];
+
+		for (let i = 0; i < numberOfRows; i++) {
+			let row = [];
+
+			for (let j = 0; j < numberOfColumns; j++) {
+				row.push(null);
+			}
+
+			board.push(row);
+		}
+
+		let numberOfBombsPlaced = 0;
+		let randomRowIndex = 0;
+		let randomColumnIndex = 0;
+
+		while (numberOfBombsPlaced < numberOfBombs) {
+			randomRowIndex = Math.floor(Math.random() * numberOfRows);
+			randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+
+			// Check to see if bomb space free. If free then place bomb, else try again
+			if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+				board[randomRowIndex][randomColumnIndex] = 'B';
+				numberOfBombsPlaced++;
+			}
+
+		}
+
+		return board;
 	}
 }
-
-/* Function to handle formatting the board for printing to the console.
- * This uses the arrow function notation from ES6, omitting the set of
- * parentheses around the parameter since there is only a single parameter.
- * Here 'row' is an array of spaces, so join works on it. The chained 'join'
- * call works to put newline char between rows.
- */
-const printBoard = board => {
-	console.log(board.map(row => {
-		return row.join(' | ');
-	}).join('\n'));
-};
 
 const rows = 4;
 const columns = 3;
 const bombs = 5;
 
-
-let playerBoard = generatePlayerBoard(rows, columns);
-let bombBoard = generateBombBoard(rows, columns, bombs);
-
-console.log('-- Player Board --')
-printBoard(playerBoard);
-console.log('--- Bomb Board ---')
-printBoard(bombBoard);
-
-flipTile(playerBoard, bombBoard, 0 , 0);
-console.log('Updated Player Board: ');
-printBoard(playerBoard);
+const g = new Game(rows, columns, bombs);
+g.playMove(1, 0);
 
